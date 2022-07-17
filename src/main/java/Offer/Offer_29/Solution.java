629ef83f6d1694922417dc48252202196af6bc4c
liujun <2218932570@qq.com>
2022-07-18 00:24:57 +0800

    2022.07.18

--- src/main/java/Offer/Offer_29/Solution.java
+++ src/main/java/Offer/Offer_29/Solution.java
@@ -1,12 +1,30 @@
 package Offer.Offer_29;
 
 public class Solution {
+//    主要是模拟 顺时针打印的顺序
     public int[] spiralOrder(int[][] matrix) {
         int rows = matrix.length;
         if (rows == 0) {
-            return null;
+            return new int[0];
         }
         int cols = matrix[0].length;
-        int len
+        int len = rows * cols;
+        int[] res = new int[len];
+        boolean[][] isVisit = new boolean[rows][cols];
+        int[][] direction = {{0,1}, {1, 0}, {0, -1},{-1, 0}};
+        int row = 0, col = 0, directionIndex = 0;
+        for (int i = 0;i < len; i++) {
+            res[i] = matrix[row][col];
+            isVisit[row][col] = true;
+            if (row + direction[directionIndex][0] >=rows || row + direction[directionIndex][0] < 0
+            || col + direction[directionIndex][1] >=cols || col + direction[directionIndex][1] < 0
+            || isVisit[row + direction[directionIndex][0]][col + direction[directionIndex][1]]) {
+                directionIndex += 1;
+                directionIndex %= 4;
+            }
+            row += direction[directionIndex][0];
+            col += direction[directionIndex][1];
+        }
+        return res;
     }
 }
